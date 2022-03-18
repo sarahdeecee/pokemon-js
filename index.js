47ce86b4021043b3285c9516d7fc7ef77f3fe076
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-03-18 13:46:48 -0400

    add map image

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const canvas = document.querySelector('canvas');
+// Create active area
 const context = canvas.getContext('2d');
 canvas.width = 1024;
 canvas.height = 576;
 
+context.fillStyle = 'white';
 context.fillRect(0, 0, canvas.width, canvas.height);
+
+// Import map image
+const image = new Image();
+image.src = './assets/map.png'
+image.onload = () => {
+  context.drawImage(image,-200,-1100)
+}
