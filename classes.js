5b604fb01de05aeb3e92d277d7076e2da35de0c8
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-03-24 15:17:16 -0400

    add animation for player sprite

--- classes.js
+++ classes.js
@@ -9,22 +9,33 @@ class Sprite {
   constructor({position, velocity, image, frames = {max: 1}}) {
     this.position = position;
     this.image = image;
-    this.frames = frames;
+    this.frames = {...frames, val: 0, elapsed: 0};
     this.image.onload = () => {
       this.width = this.image.width / this.frames.max;
       this.height = this.image.height;
     }
+    this.moving = false;
   }
   draw() {
     // context.drawImage(this.image,this.position.x,this.position.y);
     context.drawImage(
       this.image,
-      0, 0, this.image.width/this.frames.max, this.image.height,
+      this.frames.val * this.width, 0,
+      this.image.width/this.frames.max, this.image.height,
       this.position.x,
       this.position.y,
       this.image.width/this.frames.max,
       this.image.height
     );
+
+    if (!this.moving) return;
+    
+    if (this.frames.max > 1) {
+      this.frames.elapsed++;
+    }
+    if (this.frames.elapsed % 10 === 0) {
+      this.frames.val = (this.frames.val < this.frames.max - 1) ? this.frames.val + 1 : 0;
+    }
   }
 }
 
